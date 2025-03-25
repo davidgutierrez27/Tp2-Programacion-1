127996307d18179e17e0ce5965ace4dbb53ed466
David Gutierrez <davidgutierrez02027@gmail.com>
2025-03-25 18:39:24 -0300

    Agregando mi-archivo.txt

---
+++ main.py
@@ -0,0 +1,9 @@
+# Solicitar al usuario 3 números  
+num1 = float(input("Escribe primer numero: "))
+num2 = float(input("Escribe segundo numero: "))
+num3 = float(input("Escribe tercer numero: "))
+
+
+promedio = (num1 + num2 + num3) / 3
+# Imprimir resultados  
+print(f"El promedio es; {promedio:.2f}") 
